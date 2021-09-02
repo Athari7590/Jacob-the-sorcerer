2f52c6eab952f6ad84b40f9127043264138e5cdc
Athari7590 <koharyb@gmail.com>
2021-09-02 12:58:27 +0100

    fixed

--- NumberWizard.cs
+++ NumberWizard.cs
@@ -9,13 +9,15 @@ public class NumberWizard : MonoBehaviour
     int min = 1;
     int guess = 500;
 
-    // Use this for initialization
+    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to number wizard, Kkt");
-        Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
+
+
+        Debug.Log("Welcome to number wizard, Borko!");
+        Debug.Log("you supposed a pick a number ");
+        Debug.Log("Highest possible number is: " + max);
+        Debug.Log("lowest possible number it: " + min);
         Debug.Log("Tell me if your number is higher or lower than 500");
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
@@ -27,14 +29,19 @@ public class NumberWizard : MonoBehaviour
         {
             Debug.Log("Up Arrow key was pressed.");
             min = guess;
+            guess = (max + min) / 2;
             Debug.Log(guess);
+
         }
+
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed.");
             max = guess;
+            guess = (max - min) / 2;
             Debug.Log(guess);
         }
+
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("You hit enter.");
